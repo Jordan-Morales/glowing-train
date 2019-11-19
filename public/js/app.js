5d6bdab9101da4eb15eceab3fd759bfd531e235d
Jordan Morales <jordan.morales@gmail.com>
2019-11-18 18:56:36 -0800

    mostly completed MEAN minus the update route

--- public/js/app.js
+++ public/js/app.js
@@ -5,18 +5,17 @@ app.controller('MainController', ['$http', function($http){
   this.h5 = "A Mean Midnight Snack"
   this.h5_2 = "Couldn't Help Myself:"
   this.createForm = {}
+  this.snack = ''
 
-//create a snack in the DB
+  //create a snack in the DB
   this.createSnack = () => {
     $http({
       method: "POST",
       url: "/foods",
-      data: {
-        name: this.name,
-        flavor: this.flavor
-      }
+      data: this.createForm
     }).then(response => {
-      this.getSnacks();
+      this.createForm = {};
+      this.snacks.unshift(response.data);
     }, error => {
       console.log(error);
     })
@@ -33,9 +32,44 @@ app.controller('MainController', ['$http', function($http){
     })
   }
 
-  // this.updateSnack = () => {
-  //   console.lo;
+  // this.openModal = (snack) => {
+  //   let element = document.getElementById("modal");
+  //   element.classList.toggle('edit')
   // }
+
+  this.updateSnack = (snack) => {
+    // snack = this.snack
+    console.log(snack);
+    $http({
+      method: "PUT",
+      url: '/foods/' + snack._id,
+      data: {
+        name: this.updatedName,
+        flavor: this.updatedFlavor
+      }
+    }).then(response => {
+      console.log(response.data);
+      // this.openModal();
+      this.getSnacks();
+    }, error => {
+      console.log(error.message);
+    })
+  }
+
+  this.deleteSnack = snack => {
+    $http({
+      method: "DELETE",
+      url: '/foods/' + snack._id,
+    }).then(response => {
+      console.log(response.data);
+      this.getSnacks();
+    }, error => {
+      console.log(error);
+    })
+  }
+
+
+
 
   this.getSnacks();
 }]);
